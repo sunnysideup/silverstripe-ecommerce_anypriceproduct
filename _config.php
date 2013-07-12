b8119bc2c5fcd840c946fce81394ca553d55193e
astuart.pett@gmail.com <astuart.pett@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2013-07-12 02:07:16 +0000

    Upgraded to 3.1
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_anypriceproduct/trunk@4620 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -9,21 +9,5 @@
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 
 //===================---------------- START ecommerce_anypriceproduct MODULE ----------------===================
-
-// MUST SET (if you would like to use the AnyPriceRoundUpDonationModifier)
-/**
- * ADD TO ECOMMERCE.YAML:
-Order:
-	modifiers: [
-		...
-		AnyPriceRoundUpDonationModifier
-	]
-*/
-
-// MAY SET (as part of the AnyPriceRoundUpDonationModifier)
-//AnyPriceRoundUpDonationModifier::set_precision(1);
-//AnyPriceRoundUpDonationModifier::set_maximum_round_up(5);
-//AnyPriceRoundUpDonationModifier::set_round_up_even_if_there_is_nothing_to_round(true);
-//AnyPriceRoundUpDonationModifier::use_dropdown_in_modifier_form(true);
 //===================---------------- END ecommerce_anypriceproduct MODULE ----------------===================
 
