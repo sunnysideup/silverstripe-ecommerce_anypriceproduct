79ff32f6f8888e1d73aed45a663151491ddf63e0
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-10-24 14:46:40 +1300

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- code/AnyPriceProductPage.php
+++ code/AnyPriceProductPage.php
@@ -6,6 +6,20 @@
  */
 class AnyPriceProductPage extends Product
 {
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'AnyPriceProductPage';
+
     private static $db = array(
         'DescriptionFieldLabel' => 'Varchar(255)',
         'AmountFieldLabel' => 'Varchar(255)',
--- code/model/AnyPriceProductPage_ProductVariation.php
+++ code/model/AnyPriceProductPage_ProductVariation.php
@@ -3,6 +3,20 @@
 
 class AnyPriceProductPage_ProductVariation extends ProductVariation
 {
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'AnyPriceProductPage_ProductVariation';
+
     private static $db =array(
         "Description" => "Varchar(200)"
     );
